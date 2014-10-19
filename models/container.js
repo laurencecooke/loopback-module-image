ad0f118d082b893921366a13db7dee1d2b14e71c
Oleksii Strutsynskyi <cajoy1981@gmail.com>
2014-10-19 12:09:27 -0400

    filemanager initialization

--- models/container.js
+++ models/container.js
@@ -1,11 +1,17 @@
+// var fs = require('fs');
+
 module.exports = function(Container) {
   Container.afterRemote('upload', function(ctx, inst, next) {
     var models = Container.app.models;
+
+  //  var fileModule = Container.app.modules.file.getManager(app);
 
     var modelName = ctx.result.result.fields.componentId[0];
     var functionName = ctx.result.result.fields.componentClass[0];
     var targetId = ctx.result.result.fields.targetId[0];
     var file = ctx.result.result.files.file[0];
+
+//    fs.rename(fileModule.storagePath + 'import/' , '/tmp/world'
 
     models.file.create(ctx.result.result.files.file[0], function(err, data) {
       models[modelName][functionName](targetId, data)
--- module.js
+++ module.js
@@ -1,5 +1,9 @@
 module.exports = function(app) {
   var moduleClass = {
+    storagePath: __dirname + '../../storage/',
+    getManager: function () {
+      return require('./service/manager.js')(app);
+    }
   };
 
   return moduleClass;
